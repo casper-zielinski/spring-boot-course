84048794e09e94bed76a40be1750958822cc5cb2
casper-zielinski <casper.zielinski@edu.fh-joanneum.at>
2025-10-19 14:12:24 +0200

    First API, Tested GET http Method with .http file

---
+++ src/main/java/com/tavernaluna/SoftwareEngineerController.java
@@ -0,0 +1,33 @@
+package com.tavernaluna;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+
+    @GetMapping
+    public List<SoftwareEngineer> getSoftwareEngineers() {
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "James",
+                        Collections.singletonList("js, node, react, tailwindcss")
+                ),
+                (
+                        new SoftwareEngineer(
+                                2,
+                                "Maddie",
+                                Collections.singletonList("js, java, react, bootstraps, springboot")
+                        )
+                        )
+        );
+    }
+
+}
